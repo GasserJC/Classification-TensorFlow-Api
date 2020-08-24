8d0c8e8f7f86dd178722931a741e330cf8e7ad1a
Jacob Gasser <57463922+GasserJC@users.noreply.github.com>
2020-08-24 18:04:51 -0500

    Update app.py

--- app.py
+++ app.py
@@ -12,69 +12,58 @@
 #JSON encode the data
 #Return the JSON data to the browser
 
-import cgitb
-import cgi
-import json
-cgitb.enable()
+from flask import Flask
+from flask_restful import Api, Resource, request
+from flask import Response, jsonify
 
-print("Content-Type: text/plain\r\n")
+app = Flask(__name__)
+api = Api(app)
 
 def nameGuess( nameToGuess ):
    import numpy as np
    import tensorflow as tf
    from tensorflow import keras
-   model = keras.models.load_model("./NameGuess")
+   
+   print( nameToGuess )
+   model = keras.models.load_model("./NameGuess") 
+
    nmLen = 9
    predict_guess = [ nameToGuess ]
-   
-   #String Manipulation
+
    for i in range(0,len(predict_guess)):
       predict_guess[i] = predict_guess[i].zfill(nmLen)
       predict_guess[i]= predict_guess[i].lower()
    if(len(predict_guess[i]) > nmLen):
       predict_guess[i] = predict_guess[i][:nmLen]
-      
-   #Char to Int
+
    num_predict = [[0 for i in range(nmLen)] for j in range(len(predict_guess))]
+
    for i in range(0,len(predict_guess)):
       for j in range (0,nmLen):
          num_predict[i][j] = ord(predict_guess[i][j])
          num_predict[i][j] = num_predict[i][j] - 96 # EXP
-   #Int to One Hot Encoding 2-D Array
+
    num_predict = np.array(num_predict)
    num_predict = (np.arange(26) == num_predict[...,None]).astype(int)
-   
-   #Model Inference
+
    answer = model.predict(num_predict)
+
    percentMale = (answer[0][0]) * 100
    percentFemale = (answer[0][1]) * 100
+   print (percentFemale)
+   print (percentMale)
    if(percentMale <= percentFemale):
-      return((str(percentFemale) , "F"))
+      return((str(percentFemale) + "F"))
    else:
-      return((str(percentMale) , "M"))
-arguments = cgi.FieldStorage()
+      return((str(percentMale) + "M"))
 
+@app.route('/')
+def apiReturn():
+   percent = nameGuess(request.args.get("name"))
+   gender = percent[len(percent) - 1]
+   percent = percent[:len(percent) - 1]
 
-#Initial Code Execution
-try:
-   Values = nameGuess(arguments["name"].value) #Grab Arguments from URL then call TF Model Inference
-except:
-   Values = ["50.00","NA"]
-DictionaryValues = {"Probability":Values[0] ,"Gender":Values[1] } #Change TF Model Array to Dictionary
-JSONPackage = json.dumps(DictionaryValues) #Change Dictionary to JSON Package
-print(JSONPackage) #Return JSON Package
-
-
-
-
-
-
-
-
-
-
-
-
+   return jsonify(Gender=gender, Percent=percent)
 
 
 
